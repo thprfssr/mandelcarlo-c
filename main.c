ed568585beba73ac0492c476a2aad16e0e243efb
Javier Castro <jaca2899@gmail.com>
2020-11-28 18:55:42 -0500

    Defined the `is_mandelbrot()` function.

--- main.c
+++ main.c
@@ -1,10 +1,24 @@
 #include <complex.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 double complex iterate(double complex z, double complex c)
 {
 	return z*z + c;
+}
+
+bool is_mandelbrot(double complex c, unsigned int threshold)
+{
+	double complex z = 0;
+	for (unsigned int i = 0; i < threshold; i++) {
+		z = iterate(z, c);
+
+		if (cabs(z) > 2)
+			return false;
+	}
+
+	return true;
 }
 
 int main()
