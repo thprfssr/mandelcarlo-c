469931bda886093069231df5f19f521c71a0550b
Javier Castro <jaca2899@gmail.com>
2020-11-28 18:57:18 -0500

    Changed all instances of `double complex` into `long double complex`.

--- main.c
+++ main.c
@@ -3,18 +3,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-double complex iterate(double complex z, double complex c)
+long double complex iterate(long double complex z, long double complex c)
 {
 	return z*z + c;
 }
 
-bool is_mandelbrot(double complex c, unsigned int threshold)
+bool is_mandelbrot(long double complex c, unsigned int threshold)
 {
-	double complex z = 0;
+	long double complex z = 0;
 	for (unsigned int i = 0; i < threshold; i++) {
 		z = iterate(z, c);
 
-		if (cabs(z) > 2)
+		if (cabsl(z) > 2)
 			return false;
 	}
 
