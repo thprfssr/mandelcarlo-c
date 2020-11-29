60dabbd91f790aa945402804dc8ca7fccba6bc29
Javier Castro <jaca2899@gmail.com>
2020-11-28 22:26:42 -0500

    Defined the function `uniform()`, which returns a random long double taken from a uniform distribution.

--- main.c
+++ main.c
@@ -1,13 +1,22 @@
 #include <complex.h>
+#include <math.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+#define X_MIN (-2)
+#define X_MAX (0.58)
+#define Y_MIN (0)
+#define Y_MAX (1.20)
 
 long double complex iterate(long double complex z, long double complex c)
 {
 	return z*z + c;
 }
 
+/* Determines whether a given complex number is contained within the partial
+ * Mandelbrot set of a given threshold. */
 bool is_mandelbrot(long double complex c, unsigned int threshold)
 {
 	long double complex z = 0;
@@ -19,6 +28,16 @@ bool is_mandelbrot(long double complex c, unsigned int threshold)
 	}
 
 	return true;
+}
+
+/* Returns a random number from a uniform distribution.
+ *
+ * NOTE: Before calling this function, make sure to initialize the PRNG by
+ * calling `srandom(time(NULL))`. */
+long double uniform(long double a, long double b)
+{
+	long double r = (long double) random() / (long double) RAND_MAX;
+	return a + (b - a) * r;
 }
 
 int main()
