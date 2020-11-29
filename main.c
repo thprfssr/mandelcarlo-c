45af3db9b04e14fbf05f68cfdcdc938350a33ac1
Javier Castro <jaca2899@gmail.com>
2020-11-28 22:27:46 -0500

    Defined the function `integrate()`, which runs a Montecarlo integration of the N-th partial Mandelbrot set.

--- main.c
+++ main.c
@@ -40,6 +40,30 @@ long double uniform(long double a, long double b)
 	return a + (b - a) * r;
 }
 
+/* Runs a Montecarlo integration on the partial Mandelbrot set with a finite
+ * threshold. The variable `samples` determines how many points will be
+ * checked. */
+void integrate(unsigned int threshold, unsigned int samples)
+{
+	srandom(time(NULL));
+	unsigned int hits = 0;
+	for (unsigned int i = 0; i < samples; i++) {
+		long double x = uniform(X_MIN, X_MAX);
+		long double y = uniform(Y_MIN, Y_MAX);
+		long double complex c = x + I * y;
+
+		if (is_mandelbrot(c, threshold))
+			hits++;
+	}
+
+	long double rect_area = (X_MAX - X_MIN) * (Y_MAX - Y_MIN);
+	long double q = (long double) hits / (long double) samples;
+	long double A = q * rect_area;
+	long double u_A = rect_area * sqrtl(hits * (1 - q)) / samples;
+
+	printf("threshold = %u\tA = %Lf +/- %Lf\n", threshold, 2*A, 2*u_A);
+}
+
 int main()
 {
 	return 0;
